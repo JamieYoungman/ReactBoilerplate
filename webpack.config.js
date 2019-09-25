8b8babfd58eb99885db889dc9ddc1271f3ddb2fd
Jamie Youngman <jamie.youngman90@gmail.com>
2019-09-25 13:16:59 +0100

    basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+var hwp = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{ test: /\.(js)$/, use: 'babel-loader' }]
+  },
+  mode: 'development',
+  plugins: [
+    new hwp({
+      template: 'src/index.html'
+    })
+  ]
+};
